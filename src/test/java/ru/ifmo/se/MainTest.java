6a2cca829793505c47eca6d183fe1661862f346d
wieceslaw <vyache-nov@mail.ru>
2024-02-28 20:52:22 +0300

    init

---
+++ src/test/java/ru/ifmo/se/MainTest.java
@@ -0,0 +1,12 @@
+package ru.ifmo.se;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+    @Test
+    void testHelloWorld() {
+        assertTrue(true);
+    }
+}
