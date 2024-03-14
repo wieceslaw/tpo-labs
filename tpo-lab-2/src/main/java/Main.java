6904048ee23b7423741043bb67e014d2dfdb1652
AmiFideles <251645@niuitmo.ru>
2024-03-14 19:33:17 +0300

    add module for lab2

---
+++ tpo-lab-2/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static Object foo() {
+        return null;
+    }
+}
---
+++ tpo-lab-2/src/test/java/MainTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class MainTest {
+    @Test
+    void name() {
+        assertNull(Main.foo());
+    }
+}
